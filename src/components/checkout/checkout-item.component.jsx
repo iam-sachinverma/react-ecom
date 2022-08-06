98f40f4d477f07bcf647a04d0ea2b8fcec2704ad
iam-sachinverma <sachinvermab@gmail.com>
2022-08-06 16:12:43 +0530

    Decrement Item Qty Checkout 2

--- src/components/checkout/checkout-item.component.jsx
+++ src/components/checkout/checkout-item.component.jsx
@@ -7,12 +7,14 @@ import "./checkout-item.styles.scss";
 const CheckoutItem = ({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
 
-  const { addItemToCart } = useContext(CartContext);
+  const { addItemToCart, removeItemFromCart } = useContext(CartContext);
 
-  const quantityHandler = (event) => {
+  const IncrementQuantityHandler = (event) => {
     event.preventDefault();
     addItemToCart(cartItem);
   };
+
+  const DecrementQuantityHandler = () => removeItemFromCart(cartItem);
 
   return (
     <div className='checkout-item-container'>
@@ -22,11 +24,13 @@ const CheckoutItem = ({ cartItem }) => {
       <span className='name'>{name}</span>
 
       <div className='quantity'>
-        <span className='arrow' onClick={quantityHandler}>
+        <span className='arrow' onClick={IncrementQuantityHandler}>
           +
         </span>
         <span className='value'>{quantity}</span>
-        <span className='arrow'>-</span>
+        <span className='arrow' onClick={DecrementQuantityHandler}>
+          -
+        </span>
       </div>
 
       <span className='price'>{price}</span>
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -19,12 +19,32 @@ const addCartItem = (cartItems, productToAdd) => {
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
+const removeCartItem = (cartItems, cartItemToRemove) => {
+  // find cart item to remove
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+
+  // check if quantity is equal to 1 : if it is then remove item from cart
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  // is greater than 1 : then return cartItems with reduce quanity
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
   cartCount: 0,
+  removeItemFromCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
@@ -44,12 +64,17 @@ export const CartProvider = ({ children }) => {
     setCartItems(addCartItem(cartItems, productToAdd));
   };
 
+  const removeItemFromCart = (cartItemToRemove) => {
+    setCartItems(removeCartItem(cartItems, cartItemToRemove));
+  };
+
   const value = {
     isCartOpen,
     setIsCartOpen,
     addItemToCart,
     cartItems,
     cartCount,
+    removeItemFromCart,
   };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
