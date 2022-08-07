7ea9f8bd7c4f6c074dddce1a237b343ee636c6ea
iam-sachinverma <sachinvermab@gmail.com>
2022-08-07 05:59:00 +0530

    Styled-Components-Navbar

--- src/routes/navbar/navbar.component.jsx
+++ src/routes/navbar/navbar.component.jsx
@@ -11,7 +11,12 @@ import { CartContext } from "../../contexts/cart.context";
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
-import "./navbar.styles.scss";
+import {
+  NavbarContainer,
+  LogoContainer,
+  NavLinks,
+  NavLink,
+} from "./navbar.styles.jsx";
 
 const Navbar = () => {
   const { currentUser } = useContext(UserContext);
@@ -20,30 +25,26 @@ const Navbar = () => {
 
   return (
     <Fragment>
-      <div className='navigation'>
-        <Link className='logo-container' to='/'>
+      <NavbarContainer>
+        <LogoContainer to='/'>
           <CrownLogo className='logo' />
-        </Link>
+        </LogoContainer>
 
-        <div className='nav-links-container'>
-          <Link className='nav-link' to='/shop'>
-            SHOP
-          </Link>
+        <NavLinks>
+          <NavLink to='/shop'>SHOP</NavLink>
           {currentUser ? (
-            <span className='nav-link' onClick={signOutUser}>
+            <NavLink as='span' onClick={signOutUser}>
               SIGN OUT
-            </span>
+            </NavLink>
           ) : (
-            <Link className='nav-link' to='/auth'>
-              SIGN IN
-            </Link>
+            <NavLink to='/auth'>SIGN IN</NavLink>
           )}
-
           <CardIcon />
-        </div>
+        </NavLinks>
+
         {/* short circuting */}
         {isCartOpen && <CartDropdown />}
-      </div>
+      </NavbarContainer>
       <Outlet />
     </Fragment>
   );
