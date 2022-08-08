74029224dcf8284f7edbacbbce361f9487930d66
iam-sachinverma <sachinvermab@gmail.com>
2022-08-08 20:16:00 +0530

    Reducer -- UserContext

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,22 +1,57 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
 } from "../utils/firebase/firebase.utils";
 
+// context is what value we expose
 export const UserContext = createContext({
   currentUser: null,
   setCurrentUser: () => null,
 });
 
+const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state, action) => {
+  // console.log("Dispatched");
+  // console.log(action);
+
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STAGE = {
+  currentUser: null,
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  // const [currentUser, setCurrentUser] = useState(null);
+
+  const [state, dispatch] = useReducer(userReducer, INITIAL_STAGE); // reducer() and initial_stage_value
+
+  const { currentUser } = state;
+  // console.log(currentUser);
+
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+  };
+
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
-      // this is for googlesignin button
       if (user) {
         createUserDocumentFromAuth(user);
       }
@@ -28,3 +63,23 @@ export const UserProvider = ({ children }) => {
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
+
+/*
+
+const userReducer (state, action) => {
+  
+  action and state both is an obj
+
+  const { type, payload } = action
+
+  const { prevState, newlyModify State } = state
+ 
+
+  return {
+    ...state 
+    currentUser : payload
+  }
+
+}
+
+*/
