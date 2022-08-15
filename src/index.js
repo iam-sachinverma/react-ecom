b0291e25978460f8d099299bb98d1647694e2059
iam-sachinverma <sachinvermab@gmail.com>
2022-08-15 22:07:33 +0530

    Redux Persist

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/lib/integration/react";
 
 import App from "./App";
 
-import { CartProvider } from "./contexts/cart.context";
-
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
 
 import "./index.scss";
 
@@ -15,7 +14,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,16 @@
 import { compose, createStore, applyMiddleware } from "redux";
-// import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+
+import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./root-reducer";
-
-// Curring ---- fun(a,b,c) into as callable fun(a)(b)(c)
 
 const loggerMiddleware = (store) => (next) => (action) => {
   if (!action.type) {
     return next(action);
   }
 
-  // console.log("type : ", action.type);
+  console.log("type : ", action.type);
   console.log("payload : ", action.payload);
   console.log("current_state : ", store.getState());
 
@@ -19,8 +19,22 @@ const loggerMiddleware = (store) => (next) => (action) => {
   console.log("next state : ", store.getState());
 };
 
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middlewares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
